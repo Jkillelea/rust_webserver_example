6535270b086c1b584c5514239a311131b3d0c4ef
Jacob Killelea <Jkillelea@protonmail.ch>
2018-05-09 16:54:26 -0600

    Move thread pool to its own crate because RLS broke with the src/lib.rs and src/bin/main.rs. Ensure that only files in webroot get served

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-// #![allow(dead_code)]
-extern crate rust_webserver_example;
-use rust_webserver_example::ThreadPool;
+extern crate thread_pool;
+use thread_pool::ThreadPool;
 
 // use std::io;
 use std::thread;
@@ -9,6 +8,8 @@ use std::io::prelude::*;
 use std::path::{Path, PathBuf};
 use std::fs::File;
 use std::net::{TcpListener, TcpStream};
+
+const WEBROOT: &'static str = "./webroot";
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
@@ -32,30 +33,36 @@ fn handle(mut stream: TcpStream) {
 
     // parse request types
     let (status, filename) = if buf.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", PathBuf::from("hello.html"))
+        ("HTTP/1.1 200 OK\r\n\r\n", PathBuf::from(WEBROOT).join("hello.html"))
     } else if buf.starts_with(sleep) {
         thread::sleep(Duration::from_secs(5)); // wait for a bit
-        ("HTTP/1.1 200 OK\r\n\r\n", PathBuf::from("hello.html"))
+        ("HTTP/1.1 200 OK\r\n\r\n", PathBuf::from(WEBROOT).join("hello.html"))
     } else { // look for file
         // ("HTTP/1.1 404 NOT FOUND\r\n\r\n", PathBuf::from("404.html"))
-        let p = find_file(&String::from_utf8_lossy(&buf));
+        let request_path = String::from_utf8_lossy(&buf);
+        println!("request for {}", request_path.split_whitespace().nth(1).unwrap_or("[Error]"));
+        let p = find_file(&request_path);
+        
         if let Some(p) = p {
             if p.is_file() {
                 ("HTTP/1.1 200 OK\r\n\r\n", p)
             } else {
-                ("HTTP/1.1 404 NOT FOUND\r\n\r\n", PathBuf::from("404.html"))
+                ("HTTP/1.1 404 NOT FOUND\r\n\r\n", PathBuf::from(WEBROOT).join("404.html"))
             }
         } else {
-            ("HTTP/1.1 404 NOT FOUND\r\n\r\n", PathBuf::from("404.html"))
+            ("HTTP/1.1 404 NOT FOUND\r\n\r\n", PathBuf::from(WEBROOT).join("404.html"))
         }
     };
+
+    println!("serving {:?}", filename);
 
     let response = format!("{}{}\r\n", status, file_to_string(filename));
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
 
-fn file_to_string<P: AsRef<Path>>(p: P) -> String {
+fn file_to_string<P: AsRef<Path> + ::std::fmt::Debug>(p: P) -> String {
+    // println!("{:?}", p);
     let mut contents = String::new();
     let mut file = File::open(p).unwrap();
     file.read_to_string(&mut contents).unwrap();
@@ -63,15 +70,14 @@ fn file_to_string<P: AsRef<Path>>(p: P) -> String {
 }
 
 fn find_file(request: &str) -> Option<PathBuf> {
-    let server_root = PathBuf::from(".");
+    let server_root = PathBuf::from(WEBROOT); // only search within the webroot directory
     let file_path = request.split_whitespace()
                             .nth(1).unwrap();
     // println!("{}", file_path);
     let path = server_root.join(&file_path[1..]);
-    println!("Request for file {:?}", path);
-
+    // println!("Request for file {:?}", path);
     let path = path.canonicalize();
-    if path.is_err() {
+    if path.is_err() { // file doesn't exist
         return None
     } else {
         return Some(path.unwrap())
